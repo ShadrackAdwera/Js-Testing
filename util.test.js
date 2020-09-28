abb011626f42f5ce13f20675612948a75d4431c8
FACTS  LIMITED <admin@factsafrica.com>
2020-09-28 12:25:54 +0300

    run e2e test

--- util.test.js
+++ util.test.js
@@ -1,4 +1,5 @@
 const { generateText, checkValidation } = require('./util')
+const puppeteer = require('puppeteer')
 
 //Unit tests
 describe('Util.js', ()=>{
@@ -17,5 +18,26 @@ describe('Util.js', ()=>{
     it('should generate a valid text output', ()=>{
         const text = checkValidation('Adwera', 25)
         expect(text).toBe('Adwera (25 years old)')
+    })
+})
+
+//e2e test
+describe('e2e', ()=> {
+    it('should simulate a user\'s actions', async()=>{
+        const browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 80,
+            args: ['--window-size=1920,1080']
+        })
+        const page = await browser.newPage()
+        await page.goto('file:///home/adwesh/Downloads/Compressed/testing-01-starting-setup/testing-01-starting-setup/index.html')
+        await page.click('input#name')
+        await page.type('input#name','Shaddie')
+        await page.click('input#age')
+        await page.type('input#age', '24')
+        await page.click('#btnAddUser')
+        setTimeout(async()=>{
+            await browser.close()
+        },2000)
     })
 })
