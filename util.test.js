5673475bb73697853a17c81827ca8a4a27f1df71
FACTS  LIMITED <admin@factsafrica.com>
2020-09-28 12:03:56 +0300

    run integration test

--- util.test.js
+++ util.test.js
@@ -1,12 +1,21 @@
-const { generateText } = require('./util')
+const { generateText, checkValidation } = require('./util')
 
+//Unit tests
 describe('Util.js', ()=>{
-    it('Returns both name and age concatenated', ()=>{
+    it('should output both name and age', ()=>{
         const nameAge = generateText('Adwera',25)
         expect(nameAge).toBe('Adwera (25 years old)')
     })
-    it('Returns a data-less text', ()=>{
+    it('should return a data-less text', ()=>{
         const nameAge = generateText(' ',null)
         expect(nameAge).toBe('  (null years old)')
+    })
+})
+
+//Integration test
+describe('Util.js', ()=>{
+    it('should generate a valid text output', ()=>{
+        const text = checkValidation('Adwera', 25)
+        expect(text).toBe('Adwera (25 years old)')
     })
 })
